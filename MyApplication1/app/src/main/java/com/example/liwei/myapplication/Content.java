e91f3681855a9fc5104f32f5efd69ab78ec203d8
xiaowei20152012 <liweibreak2012@163.com>
2015-11-16 20:47:38 +0800

    kjjj

---
+++ MyApplication1/app/src/main/java/com/example/liwei/myapplication/Content.java
@@ -0,0 +1,11 @@
+package com.example.liwei.myapplication;
+
+/**
+ * Created by liwei on 15/10/19.
+ */
+public class Content {
+
+    public static final String TAG= Content.class.getSimpleName();
+
+
+}
---
+++ MyApplication1/app/src/main/java/com/example/liwei/myapplication/MainActivity2ActivityFragment.java
@@ -0,0 +1,34 @@
+package com.example.liwei.myapplication;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivity2ActivityFragment extends Fragment {
+
+    public MainActivity2ActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main_activity2, container, false);
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+}
